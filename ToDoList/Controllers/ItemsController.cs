c42c13aefb35945b1ab14c69b605dffc3013d4a8
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-05-12 17:56:37 -0500

    edit project updated to use viewbag but the add category button is boken

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -3,7 +3,7 @@ using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ToDoList.Controllers
 {
@@ -18,36 +18,34 @@ namespace ToDoList.Controllers
 
     public ActionResult Index()
     {
-      List<Item> model = _db.Items.ToList();
+      List<Item> model = _db.Items.Include(items => items.Category).ToList();
       return View(model);
     }
+
     public ActionResult Create()
     {
-        return View();
-    }
-
-    public ActionResult Edit(int id)
-    {
-      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-      return View(thisItem);
-    }
-    public ActionResult Delete(int id)
-    {
-      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-      return View(thisItem);
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View();
     }
 
     [HttpPost]
     public ActionResult Create(Item item)
     {
-        _db.Items.Add(item);
-        _db.SaveChanges();
-        return RedirectToAction("Index");
+      _db.Items.Add(item);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
 
     public ActionResult Details(int id)
     {
-      Item thisItem = _db.Items.FirstOrDefault(Item => Item.ItemId == id);
+      Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      return View(thisItem);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
       return View(thisItem);
     }
 
@@ -59,20 +57,19 @@ namespace ToDoList.Controllers
       return RedirectToAction("Index");
     }
 
+    public ActionResult Delete(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      return View(thisItem);
+    }
+
     [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
     {
-      var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
       _db.Items.Remove(thisItem);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
   }
-
-
-//   public ActionResult Details(int id)
-// {
-//     Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-//     return View(thisItem);
-// }
 }
